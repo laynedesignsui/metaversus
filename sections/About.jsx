c07ae7072052f25d091124ec739d2f5ed22cac22
laynedesignsui <112714618+laynedesignsui@users.noreply.github.com>
2023-10-27 17:26:34 -0400

    second commit

---
+++ sections/About.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} ${styles.flexCenter} flex-col mx-auto`}
+    >
+      
+    </motion.div>
+  </section>
+);
+
+export default About;
